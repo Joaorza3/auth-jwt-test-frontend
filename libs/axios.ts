f03e67aa967238b1127a2e5d9a8ad40839974a8b
joaorza3yarn <rozenaldofilho@gmail.com>
2024-10-31 07:55:26 -0300

    chore: changed readme

--- libs/axios.ts
+++ libs/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost/auth-api',
+    baseURL: 'http://localhost:8000',
 });
 
 export const setAxiosAuthToken = (token: string) => instance.defaults.headers.common['Authorization'] = token;
